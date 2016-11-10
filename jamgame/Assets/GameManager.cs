7ae47f31034cbbcaa920af0c28f4afcc4ebd917a
Jordan B <jordbej@yahoo.com>
2016-11-09 23:48:01 -0600

    major code update
    major code updates

---
+++ jamgame/Assets/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    public static GameObject SelectedObject { get; private set; }
+    private static InteractionBehavior selectedBehav { get; set; }
+    public static Vector3 PrevMousePosition { get; private set; }
+    
+    public static void AssignSelectedObject( GameObject obj )
+    {
+        SelectedObject = obj;
+        if ( SelectedObject.GetComponent<InteractionBehavior>() != null)
+        {
+            selectedBehav = SelectedObject.GetComponent<InteractionBehavior>();
+        }
+    }
+
+    public static void Deselect()
+    {
+        if( selectedBehav != null )
+        {
+            selectedBehav = null;
+        }
+        SelectedObject = null;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if( SelectedObject != null )
+        {
+
+            if( selectedBehav != null )
+            {
+                selectedBehav.UpdateRotation();
+            }
+        }
+
+        if( Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            PrevMousePosition = Input.mousePosition;
+            Ray ray = Camera.main.ScreenPointToRay(PrevMousePosition);
+            
+            if (Physics.Raycast(ray, out hit))
+            {
+                AssignSelectedObject(hit.transform.gameObject);
+            }
+        }
+        else if ( Input.GetButtonUp("Fire1"))
+        {
+            Deselect();
+        }
+	}
+}
---
+++ jamgame/Assets/InteractionBehavior.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class InteractionBehavior : MonoBehaviour {
+
+    public enum State { Increasing, Amass, Decreasing, Original }
+    public State state = State.Original;
+    public bool IsTouching;
+    private Vector3 orgScale;
+    private Quaternion orgRotation;
+    public float ScaleFactor = 3f;
+    public float duration = 0.5f;
+    public AnimationCurve AnimCurve = new AnimationCurve();
+    private float i;
+    private Rigidbody rb;
+
+    private float x;
+    private float y;
+
+    public bool returnToOriginalRotation;
+
+	// Use this for initialization
+	void Start ()
+    {
+        orgScale = transform.localScale;
+        orgRotation = transform.rotation;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void OnStartTouch()
+    {
+        IsTouching = true;
+    }
+
+    public void OnEndTouch()
+    {
+        IsTouching = false;
+    }
+
+    // rotate the object based from the camera orientation.. How??? Local space?
+    // what about angle forces? 
+    public void UpdateRotation()
+    {
+        Vector3 currMousePos = Input.mousePosition;
+        Vector3 OrgRotEul = orgRotation.eulerAngles;
+        Vector3 DeltaPos = new Vector3( OrgRotEul.x + currMousePos.y - GameManager.PrevMousePosition.y,
+                                        OrgRotEul.y + currMousePos.x + GameManager.PrevMousePosition.x,
+                                        OrgRotEul.z + currMousePos.z - GameManager.PrevMousePosition.z);
+        transform.rotation = Quaternion.Euler(DeltaPos);
+
+    }
+
+	// Update is called once per frame
+
+        // if we want the cube to return to it's original rotation then we need to add something in here that will help us utilize that.
+	void FixedUpdate ()
+    {
+        IsTouching = this.gameObject == GameManager.SelectedObject;
+        
+        // we would have to do a switch statement here. 
+        if( IsTouching )
+        {
+            i += Time.fixedDeltaTime / duration;
+        }   
+        else
+        {
+            // return back to normal if possible
+            i -= Time.fixedDeltaTime / duration;
+            if ( returnToOriginalRotation )
+            {
+                if( transform.rotation != orgRotation )
+                {
+                    rb.angularVelocity = Vector3.zero;
+                    transform.rotation = Quaternion.Slerp(transform.rotation, orgRotation, Time.fixedDeltaTime * 10f);
+                }
+            }
+        }     
+        
+        i = Mathf.Clamp01(i);
+        transform.localScale = Vector3.Lerp(orgScale, orgScale * ScaleFactor, AnimCurve.Evaluate(i));
+    }
+}
