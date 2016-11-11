97c365cf52bfdb82db1b79e920127c79b0d8ff1d
Regretful123 <jordbej@yahoo.com>
2016-11-11 00:09:43 -0600

    Merge remote-tracking branch 'origin/master'
    Improve script and updated information for selection
    game manager now has delegate to call out for On Start Selected and On
    End Selected, just find a way to make it work for other objects

--- jamgame/Assets/GameManager.cs
+++ jamgame/Assets/GameManager.cs
@@ -1,54 +1,80 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class GameManager : MonoBehaviour {
-    public static GameObject SelectedObject { get; private set; }
-    private static InteractionBehavior selectedBehav { get; set; }
-    public static Vector3 PrevMousePosition { get; private set; }
-    
-    public static void AssignSelectedObject( GameObject obj )
+namespace Jordan
+{
+    public class GameManager : MonoBehaviour
     {
-        SelectedObject = obj;
-        if ( SelectedObject.GetComponent<InteractionBehavior>() != null)
-        {
-            selectedBehav = SelectedObject.GetComponent<InteractionBehavior>();
-        }
-    }
+        public static GameObject SelectedObject { get; private set; }
+        private static InteractionBehavior selectedBehav { get; set; }
+        public static Vector3 PrevMousePosition { get; set; }
 
-    public static void Deselect()
-    {
-        if( selectedBehav != null )
-        {
-            selectedBehav = null;
-        }
-        SelectedObject = null;
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        if( SelectedObject != null )
-        {
+        public delegate void OnEndSelect(object sender, GameObject e);
+        public delegate void OnStartSelect(object sender, GameObject e);
+        private static GameManager instance;
+        public static GameManager Instance { get { return instance ?? new GameObject("GameManager").AddComponent<GameManager>(); } }
+        public event OnEndSelect EndSelected;
+        public event OnStartSelect StartSelected;
 
-            if( selectedBehav != null )
+        protected void NotifyStartSelectedEvent(GameObject obj)
+        {
+            if (StartSelected != null)
+            { StartSelected(this, obj); }
+        }
+
+        protected void NotifyEndSelectedEvent(GameObject obj)
+        {
+            if (EndSelected != null)
+            { EndSelected(this, obj); }
+        }
+
+        protected void Start()
+        {
+            if (instance == null)
+            { instance = this; }
+        }
+
+        public static void AssignSelectedObject(GameObject obj)
+        {
+            SelectedObject = obj;
+            if (SelectedObject.GetComponent<InteractionBehavior>() != null)
+                selectedBehav = SelectedObject.GetComponent<InteractionBehavior>();
+            Instance.NotifyStartSelectedEvent(SelectedObject);
+        }
+
+        public static void Deselect()
+        {
+            if (selectedBehav != null) selectedBehav = null;
+            Instance.NotifyEndSelectedEvent(SelectedObject);
+            SelectedObject = null;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (SelectedObject != null)
             {
-                selectedBehav.UpdateRotation();
+
+                if (selectedBehav != null)
+                {
+                    selectedBehav.UpdateRotation();
+                }
+            }
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                RaycastHit hit;
+                PrevMousePosition = Input.mousePosition;
+                Ray ray = Camera.main.ScreenPointToRay(PrevMousePosition);
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    AssignSelectedObject(hit.transform.gameObject);
+                }
+            }
+            else if (Input.GetButtonUp("Fire1"))
+            {
+                Deselect();
             }
         }
-
-        if( Input.GetButtonDown("Fire1"))
-        {
-            RaycastHit hit;
-            PrevMousePosition = Input.mousePosition;
-            Ray ray = Camera.main.ScreenPointToRay(PrevMousePosition);
-            
-            if (Physics.Raycast(ray, out hit))
-            {
-                AssignSelectedObject(hit.transform.gameObject);
-            }
-        }
-        else if ( Input.GetButtonUp("Fire1"))
-        {
-            Deselect();
-        }
-	}
+    }
 }
--- jamgame/Assets/InteractionBehavior.cs
+++ jamgame/Assets/InteractionBehavior.cs
@@ -1,83 +1,102 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-[RequireComponent(typeof(Rigidbody))]
-public class InteractionBehavior : MonoBehaviour {
-
-    public enum State { Increasing, Amass, Decreasing, Original }
-    public State state = State.Original;
-    public bool IsTouching;
-    private Vector3 orgScale;
-    private Quaternion orgRotation;
-    public float ScaleFactor = 3f;
-    public float duration = 0.5f;
-    public AnimationCurve AnimCurve = new AnimationCurve();
-    private float i;
-    private Rigidbody rb;
-
-    private float x;
-    private float y;
-
-    public bool returnToOriginalRotation;
-
-	// Use this for initialization
-	void Start ()
+namespace Jordan
+{
+    public class InteractionBehavior : MonoBehaviour
     {
-        orgScale = transform.localScale;
-        orgRotation = transform.rotation;
-        rb = GetComponent<Rigidbody>();
-    }
 
-    public void OnStartTouch()
-    {
-        IsTouching = true;
-    }
+        public enum State { Increasing, Amass, Decreasing, Original }
+        public State state = State.Original;
+        public bool IsTouching;
+        private Vector3 orgScale;
+        private Quaternion orgRotation;
+        private Quaternion targetRotation;
+        public float ScaleFactor = 3f;
+        public float duration = 0.5f;
+        public AnimationCurve AnimCurve = new AnimationCurve();
+        private float i;
+        public bool returnToOriginalRotation;
 
-    public void OnEndTouch()
-    {
-        IsTouching = false;
-    }
+        // Use this for initialization
+        void Start()
+        {
+            orgScale = transform.localScale;
+            orgRotation = transform.rotation;
+        }
 
-    // rotate the object based from the camera orientation.. How??? Local space?
-    // what about angle forces? 
-    public void UpdateRotation()
-    {
-        Vector3 currMousePos = Input.mousePosition;
-        Vector3 OrgRotEul = orgRotation.eulerAngles;
-        Vector3 DeltaPos = new Vector3( OrgRotEul.x + currMousePos.y - GameManager.PrevMousePosition.y,
-                                        OrgRotEul.y + currMousePos.x + GameManager.PrevMousePosition.x,
-                                        OrgRotEul.z + currMousePos.z - GameManager.PrevMousePosition.z);
-        transform.rotation = Quaternion.Euler(DeltaPos);
+        /// <summary>
+        /// Return a better linear interpolate between two vectors without having to clamp between zero and one
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        private Vector3 Interpolate(Vector3 a, Vector3 b, float t)
+        {
+            return t * b + (1 - t) * a;
+        }
 
-    }
+        // why won't this script work???
+        void OnStartSelect(object sender, GameObject e)
+        {
+            Debug.Log("Start Select method is called from game manager within InteractionBehavior script!");
+        }
 
-	// Update is called once per frame
+        void OnEndSelect(object sender, GameObject e)
+        {
+            Debug.Log("End Select method is called from game manager within InteractionBehavior script!");
+            // find a way to get this function called!!!
+            if (this.gameObject == e)
+            {
+                // make the cube faced correctly or face towards the world snapped.
+            }
+        }
+
+        // rotate the object based from the camera orientation.. How??? Local space??? The code below only works on local orientation!!! FUCK
+        // what about angle forces? 
+        public void UpdateRotation()
+        {
+            Vector3 currMousePos = Input.mousePosition;
+            float xOrt = currMousePos.x - GameManager.PrevMousePosition.x;
+            float yOrt = currMousePos.y - GameManager.PrevMousePosition.y;
+            transform.Rotate(-Vector3.up, xOrt, Space.World);
+            transform.Rotate(Vector3.right, yOrt, Space.World);
+            // update new coordinate
+            GameManager.PrevMousePosition = Input.mousePosition;
+        }
+
+        // Update is called once per frame
 
         // if we want the cube to return to it's original rotation then we need to add something in here that will help us utilize that.
-	void FixedUpdate ()
-    {
-        IsTouching = this.gameObject == GameManager.SelectedObject;
-        
-        // we would have to do a switch statement here. 
-        if( IsTouching )
+        void FixedUpdate()
         {
-            i += Time.fixedDeltaTime / duration;
-        }   
-        else
-        {
-            // return back to normal if possible
-            i -= Time.fixedDeltaTime / duration;
-            if ( returnToOriginalRotation )
+            IsTouching = this.gameObject == GameManager.SelectedObject;
+
+            // we would have to do a switch statement here. 
+            if (IsTouching)
             {
-                if( transform.rotation != orgRotation )
+                i += Time.fixedDeltaTime / duration;
+            }
+            else
+            {
+                // return back to normal if possible
+                i -= Time.fixedDeltaTime / duration;
+                if (returnToOriginalRotation)
                 {
-                    rb.angularVelocity = Vector3.zero;
-                    transform.rotation = Quaternion.Slerp(transform.rotation, orgRotation, Time.fixedDeltaTime * 10f);
+                    if (transform.rotation != orgRotation)
+                    {
+                        //rb.angularVelocity = Vector3.zero;
+                        transform.rotation = Quaternion.Slerp(transform.rotation, orgRotation, Time.fixedDeltaTime * 10f);
+                    }
+                }
+                else
+                {
+
                 }
             }
-        }     
-        
-        i = Mathf.Clamp01(i);
-        transform.localScale = Vector3.Lerp(orgScale, orgScale * ScaleFactor, AnimCurve.Evaluate(i));
+
+            i = Mathf.Clamp01(i);
+            transform.localScale = Interpolate(orgScale, orgScale * ScaleFactor, AnimCurve.Evaluate(i));
+        }
     }
 }
