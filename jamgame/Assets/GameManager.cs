dc1828bcd04f7ee68b11073de2043adef00d939d
Regretful123 <jordbej@yahoo.com>
2016-11-10 21:05:36 -0600

    Remove temp and library files
    Remove excessive files that aren't suppose to be in the repository
    system - JB

---
+++ jamgame/Assets/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    private int scaleRatio = 4;
+    public GameObject obj_Background;
+
+    // find a way to spawn objects into array of list. 
+    // preferrably withhin the distance of the camera viewport. 
+
+
+    void Awake()
+    {
+        //if (obj_Background == null )
+        Camera cam = Camera.main;
+        Vector3 TopRight = cam.ScreenToWorldPoint(new Vector3(0, cam.pixelHeight, cam.nearClipPlane));
+        Vector3 Bottomleft = cam.ScreenToWorldPoint(new Vector3(cam.pixelWidth, 0, cam.nearClipPlane));
+        
+        // shows the boundary of top left corner, and the width/height of the dimension
+        Vector4 bound = new Vector4( );
+
+        float sizeset = obj_Background.transform.localScale.magnitude * scaleRatio;
+
+        for ( int i = 0; i <  )
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
